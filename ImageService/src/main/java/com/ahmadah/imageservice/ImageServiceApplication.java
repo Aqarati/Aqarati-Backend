0642c7fb47db627b6d2111e64f6625bf5d1b6a8d
AhmdFahad <ahmd66699@gmail.com>
2024-03-31 14:52:30 +0300

    create Image Service

---
+++ ImageService/src/main/java/com/ahmadah/imageservice/ImageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ahmadah.imageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageServiceApplication.class, args);
+    }
+
+}
