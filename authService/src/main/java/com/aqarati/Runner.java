25fc388abd60950f9722a60df82471b56e2f36bd
AhmdFahad <ahmd66699@gmail.com>
2024-04-06 17:42:38 +0300

    implement a simple method to create admin user
    it`s horrible

--- authService/src/main/java/com/aqarati/Runner.java
+++ authService/src/main/java/com/aqarati/Runner.java
@@ -1,10 +1,12 @@
 package com.aqarati;
 
+import com.aqarati.model.UserApp;
 import com.aqarati.repository.UserRepository;
 import com.aqarati.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -20,10 +22,26 @@ public class Runner implements CommandLineRunner {
 
     private final AuthService authService;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public void run(String... args) throws Exception {
+//       var user=createAdminUser();
+//        userRepository.save(user);
+//        logger.info("crated user "+user.toString());
         logger.info("Active Profile");
         logger.info(activeProfile);
-    }
 
+
+    }
+    private UserApp createAdminUser(String email,String username,String password,String phoneNumber){
+        var adminUser= UserApp.builder().
+                email(email)
+                .uname(username)
+                .password(passwordEncoder.encode(password))
+                .phoneNumber(phoneNumber)
+                .build();
+        adminUser.getRole().add("ROLE_ADMIN");
+        return adminUser;
+    }
 }
