ca7c67f778084a78111afa5c03eaf355d65c3e06
AhmdFahad <ahmd66699@gmail.com>
2024-06-18 23:26:55 +0300

    connect to euruka

--- chat-service/go.mod
+++ chat-service/go.mod
@@ -1,3 +1,8 @@
 module chat-service
 
 go 1.21.0
+
+require (
+	github.com/xuanbo/eureka-client v0.0.5 // indirect
+	github.com/xuanbo/requests v0.0.1 // indirect
+)
--- chat-service/main.go
+++ chat-service/main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+
+	eureka "github.com/xuanbo/eureka-client"
 )
 
 func main() {
+
 	fmt.Println("tsest")
+	registerWithEureka()
+}
+
+func registerWithEureka() {
+	client := eureka.NewClient(&eureka.Config{
+		DefaultZone:           "http://localhost:8761/eureka/",
+		App:                   "chat-service",
+		Port:                  8080,
+		RenewalIntervalInSecs: 30,
+		DurationInSecs:        90,
+	})
+
+	client.Start()
+	http.HandleFunc("/v1/services", func(writer http.ResponseWriter, request *http.Request) {
+		// full applications from eureka server
+		apps := client.Applications
+
+		b, _ := json.Marshal(apps)
+		_, _ = writer.Write(b)
+	})
+
+	// start http server
+	if err := http.ListenAndServe(":10000", nil); err != nil {
+		fmt.Println(err)
+	}
 }
